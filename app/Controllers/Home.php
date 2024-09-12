ed1525ea4ee259970b3780a5921c5efc8532bf7d
H4zlq <haziqnaim72@gmail.com>
2024-09-12 17:17:55 +0800

    Refactor routes to add authentication filters and implement user authentication

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,7 +6,9 @@ class Home extends BaseController
 {
     public function home(): string
     {
-        return view('layout/header')
+        return view('layout/header', [
+            'route' => 'home',
+        ])
             . view('home')
             . view('layout/footer');
     }
