8fed4db65e2949bd9a3f653f5aece5a7bba63892
H4zlq <haziqnaim72@gmail.com>
2024-09-12 17:17:47 +0800

    Refactor routes to add authentication filters

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,9 +1,10 @@
 <?php
 
 use App\Controllers\TaskController;
-use App\Controllers\DashboardController;
 use App\Controllers\AuthController;
 use App\Controllers\Home;
+use App\Filters\Auth;
+use CodeIgniter\Filters\CSRF;
 use CodeIgniter\Router\RouteCollection;
 
 /**
@@ -11,12 +12,36 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', [Home::class, 'home']);
 $routes->get('/home', [Home::class, 'home']);
-$routes->get('/dashboard', [DashboardController::class, 'index']);
+$routes->get('/dashboard', [TaskController::class, 'index'], ['filter' => Auth::class]);
 
 # Auth routes
 $routes->get('/login', [AuthController::class, 'login']);
+$routes->get('/logout', [AuthController::class, 'logout']);
 $routes->get('/register', [AuthController::class, 'register']);
-$routes->get('/profile', [AuthController::class, 'profile']);
+$routes->get('/profile', [AuthController::class, 'profile'], ['filter' => Auth::class]);
 
-# Task routes
-$routes->get('/task/add', [TaskController::class, 'add']);
+/**
+ * Task routes
+ * 
+ * @param RouteCollection $routes
+ */
+$routes->group('task', function ($routes) {
+  $routes->get('add', [TaskController::class, 'add']);
+  $routes->get('edit/(:num)', [TaskController::class, 'edit/$1']);
+  $routes->post('add', [TaskController::class, 'handle_add_task']);
+  $routes->post('edit/(:num)', [TaskController::class, 'handle_edit_task/$1']);
+  $routes->get('delete/(:num)', [TaskController::class, 'handle_delete_task/$1']);
+}, ['filter' => Auth::class]);
+
+/**
+ * Form submission routes
+ * 
+ * @param RouteCollection $routes
+ */
+$routes->group('auth', function ($routes) {
+  $routes->post('login', [AuthController::class, 'handle_login']);
+  $routes->post('register', [AuthController::class, 'handle_register']);
+}, ['filter' => [
+  Auth::class,
+  CSRF::class
+]]);
